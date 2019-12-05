2c5e19e16db66909e6723235d54a46936987ead9
Alice Thammavong <thammavongalice@gmail.com>
2019-12-05 16:07:54 +0100

    Add Lorem ipsum

--- frontend/src/components/EnSavoirPlus.jsx
+++ frontend/src/components/EnSavoirPlus.jsx
@@ -5,28 +5,46 @@ export default function EnSavoirPlus() {
   return (
     <div>
       <section id="article">
-        <article className="box">
+        <article
+          className="box" //onClick TODO
+        >
           <img
             src="https://via.placeholder.com/500x200"
             alt="Article de presse"
           ></img>
-          <p>On parle de nous</p>
+          <p id="titre">On parle de nous</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
           <p id="lirePlus">Lire plus...</p>
         </article>
-        <article className="box">
+        <article
+          className="box" //onClick TODO
+        >
           <img
             src="https://via.placeholder.com/500x200"
             alt="Charte éthique"
           ></img>
-          <p>Notre charte éthique</p>
+          <p id="titre">Notre charte éthique</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
           <p id="lirePlus">Lire plus...</p>
         </article>
-        <article className="box">
+        <article
+          className="box" //onClick TODO
+        >
           <img
             src="https://via.placeholder.com/500x200"
             alt="Dons collectés"
           ></img>
-          <p>Les dons collectés</p>
+          <p id="titre">Les dons collectés</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
           <p id="lirePlus">Lire plus...</p>
         </article>
       </section>
