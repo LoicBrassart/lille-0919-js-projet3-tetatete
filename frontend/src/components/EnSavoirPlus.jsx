456973b4ffcad61fedfd9dd9542dac99216545cb
Alice Thammavong <thammavongalice@gmail.com>
2019-12-05 17:10:47 +0100

    Add </hr>, TODO Onclick

--- frontend/src/components/EnSavoirPlus.jsx
+++ frontend/src/components/EnSavoirPlus.jsx
@@ -5,35 +5,44 @@ export default function EnSavoirPlus() {
   return (
     <div>
       <section id="article">
-        <article
-          className="box" //onClick TODO
-        >
+        <article className="box">
           <p id="titre">On parle de nous</p>
+          <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">En savoir plus...</p>
+          <p
+            id="lirePlus" //onClick TODO
+          >
+            En savoir plus...
+          </p>
         </article>
-        <article
-          className="box" //onClick TODO
-        >
+        <article className="box">
           <p id="titre">Notre charte éthique</p>
+          <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">En savoir plus...</p>
+          <p
+            id="lirePlus" //onClick TODO
+          >
+            En savoir plus...
+          </p>
         </article>
-        <article
-          className="box" //onClick TODO
-        >
+        <article className="box">
           <p id="titre">Les dons collectés</p>
+          <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">En savoir plus...</p>
+          <p
+            id="lirePlus" //onClick TODO
+          >
+            En savoir plus...
+          </p>
         </article>
       </section>
     </div>
