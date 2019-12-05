dddb6951642019f26ada7079dedbc1e59b67c9bc
Alice Thammavong <thammavongalice@gmail.com>
2019-12-05 12:37:23 +0100

    Add 3 boxes+title

--- frontend/src/components/EnSavoirPlus.jsx
+++ frontend/src/components/EnSavoirPlus.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import "./styles/EnSavoirPlus.scss";
 
 export default function EnSavoirPlus() {
-  return;
+  return (
+    <div>
+      <section id="article">
+        <article className="box">
+          <img />
+          On parle de nous
+        </article>
+        <article className="box">Notre charte éthique</article>
+        <article className="box">Les dons collectés</article>
+      </section>
+    </div>
+  );
 }
