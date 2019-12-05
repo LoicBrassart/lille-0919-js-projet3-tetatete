654d1b2f6a6d29815b7698e5c47eb3c8655cb6f3
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-12-05 11:50:36 +0100

    Add component Hero

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/Hero.scss";
+
+function Hero() {
+  return (
+    <img
+      src="https://via.placeholder.com/2500x600?text=Insert+Image"
+      alt="image hero"
+    />
+  );
+}
+
+export default Hero;
