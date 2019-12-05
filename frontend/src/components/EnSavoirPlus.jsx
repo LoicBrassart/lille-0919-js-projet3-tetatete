b1d725007aa7c94ef814647ba94a9b1c606c9b56
Alice Thammavong <thammavongalice@gmail.com>
2019-12-05 16:42:09 +0100

    Delete image

--- frontend/src/components/EnSavoirPlus.jsx
+++ frontend/src/components/EnSavoirPlus.jsx
@@ -8,44 +8,32 @@ export default function EnSavoirPlus() {
         <article
           className="box" //onClick TODO
         >
-          <img
-            src="https://via.placeholder.com/500x200"
-            alt="Article de presse"
-          ></img>
           <p id="titre">On parle de nous</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">Lire plus...</p>
+          <p id="lirePlus">En savoir plus...</p>
         </article>
         <article
           className="box" //onClick TODO
         >
-          <img
-            src="https://via.placeholder.com/500x200"
-            alt="Charte éthique"
-          ></img>
           <p id="titre">Notre charte éthique</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">Lire plus...</p>
+          <p id="lirePlus">En savoir plus...</p>
         </article>
         <article
           className="box" //onClick TODO
         >
-          <img
-            src="https://via.placeholder.com/500x200"
-            alt="Dons collectés"
-          ></img>
           <p id="titre">Les dons collectés</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p id="lirePlus">Lire plus...</p>
+          <p id="lirePlus">En savoir plus...</p>
         </article>
       </section>
     </div>
