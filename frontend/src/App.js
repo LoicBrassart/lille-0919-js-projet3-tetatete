5a1c1344d257163422c981a819b376e1277c770c
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-06 14:57:15 +0100

    import all pages components + made switch route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "./App.scss";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Meetings from "./pages/Meetings";
+import Ambassadors from "./pages/Ambassadors";
+import Causes from "./pages/Causes";
+import Blog from "./pages/Blog";
+import { Switch, Route } from "react-router-dom";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/meetings" component={Meetings} />
+        <Route path="/ambassadors" component={Ambassadors} />
+        <Route path="/causes" component={Causes} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
     </div>
   );
 }
