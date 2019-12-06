ef2bd5c8f1db492d86f8f887de6041fb90dad808
valentin <valentin.faucon@gmail.com>
2019-12-06 14:40:19 +0100

    ProfilCard component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import Profilecard from "./components/ProfileCard"
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Profilecard />
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/components/ProfileCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/ProfileCard.scss";
+
+function ProfileCard() {
+  return <div className="profilecard"></div>;
+}
+
+export default ProfileCard;
