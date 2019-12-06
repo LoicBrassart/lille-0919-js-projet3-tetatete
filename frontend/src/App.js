ce1521e2bead6e837410af7e0961f84c9cf8e651
valentin <valentin.faucon@gmail.com>
2019-12-06 16:07:42 +0100

    profilecard component done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import "./App.css";
-import Profilecard from "./components/ProfileCard"
+import "./App.scss";
 
 function App() {
   return <div className="App">
-    <Profilecard />
   </div>;
 }
 
--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./styles/ProfileCard.scss";
 
 function ProfileCard() {
-  return <div className="profilecard"></div>;
+  return (
+    <div className="profileCard">
+      <img
+        classname="profileImage"
+        src="https://via.placeholder.com/300x300"
+        alt="..."
+      />
+
+      <span className="gradientName" />
+
+      <h2 className="profileName">Lorem Ipsum</h2>
+
+      <img
+        className="profileTag"
+        src="https://via.placeholder.com/50x50"
+        alt="..."
+      />
+    </div>
+  );
 }
 
 export default ProfileCard;
