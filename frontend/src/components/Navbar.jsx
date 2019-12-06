acb70c0986872b581c478e254bc4818ed70d2cb8
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-06 16:56:41 +0100

    add a div to give a good z-index priority when we open mobile menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./styles/Navbar.scss";
 
 function Navbar() {
+  const [menuIsOpen, setOpen] = useState(false);
   return (
-    <nav>
-      <div className="navbarContainer">
-        <NavLink className="logoAndTitle" exact to="/">
+    <div className="TopBar">
+      <nav>
+        <div className="navbarContainer">
+          <NavLink className="logoAndTitle" exact to="/">
+            <p>tttt</p>
+            <h1>TETATETE</h1>
+          </NavLink>
+
+          <div className="menu">
+            <NavLink activeClassName="active" to="/meetings">
+              Rencontres
+            </NavLink>
+            <NavLink activeClassName="active" to="/ambassadors">
+              Ambassadeurs
+            </NavLink>
+            <NavLink activeClassName="active" to="/causes">
+              Causes soutenues
+            </NavLink>
+            <NavLink activeClassName="active" to="/blog">
+              Blog
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="mobileNavbar">
           <p>tttt</p>
           <h1>TETATETE</h1>
-        </NavLink>
-
-        <div className="menu">
-          <NavLink activeClassName="active" to="/meetings">
-            Rencontres
-          </NavLink>
-          <NavLink activeClassName="active" to="/ambassadors">
-            Ambassadeurs
-          </NavLink>
-          <NavLink activeClassName="active" to="/causes">
-            Causes soutenues
-          </NavLink>
-          <NavLink activeClassName="active" to="/blog">
-            Blog
-          </NavLink>
+          <div
+            className="burger"
+            onClick={() => {
+              setOpen(!menuIsOpen);
+            }}
+          >
+            <span className={menuIsOpen ? "isCrossed" : ""}></span>
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className="mobileNavbar">
-        <p>tttt</p>
-        <h1>TETATETE</h1>
-        <img src="./img/burger-icon.png" alt="burger-icon" />
+      <div className={menuIsOpen ? "menuMobile isOpen" : "menuMobile"}>
+        <NavLink activeClassName="active" to="/meetings">
+          Rencontres
+        </NavLink>
+        <NavLink activeClassName="active" to="/ambassadors">
+          Ambassadeurs
+        </NavLink>
+        <NavLink activeClassName="active" to="/causes">
+          Causes soutenues
+        </NavLink>
+        <NavLink activeClassName="active" to="/blog">
+          Blog
+        </NavLink>
       </div>
-    </nav>
+    </div>
   );
 }
 
