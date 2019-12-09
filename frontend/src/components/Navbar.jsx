9f65d80792318a25682cea09a9602b598efe067a
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-09 09:24:54 +0100

    import {siteTtile} and replace the title

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./styles/Navbar.scss";
+import { siteTitle } from "../conf.js";
 
 function Navbar() {
-  const [menuIsOpen, setOpen] = useState(false);
+  const [IsMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="TopBar">
       <nav>
         <div className="navbarContainer">
           <NavLink className="logoAndTitle" exact to="/">
             <p>tttt</p>
-            <h1>TETATETE</h1>
+            <h1>{siteTitle}</h1>
           </NavLink>
 
           <div className="menu">
@@ -31,19 +32,19 @@ function Navbar() {
 
         <div className="mobileNavbar">
           <p>tttt</p>
-          <h1>TETATETE</h1>
+          <h1>{siteTitle}</h1>
           <div
             className="burger"
             onClick={() => {
-              setOpen(!menuIsOpen);
+              setOpen(!isMenuOpen);
             }}
           >
-            <span className={menuIsOpen ? "isCrossed" : ""}></span>
+            <span className={isMenuOpen ? "isCrossed" : ""}></span>
           </div>
         </div>
       </nav>
 
-      <div className={menuIsOpen ? "menuMobile isOpen" : "menuMobile"}>
+      <div className={isMenuOpen ? "menuMobile isOpen" : "menuMobile"}>
         <NavLink activeClassName="active" to="/meetings">
           Rencontres
         </NavLink>
