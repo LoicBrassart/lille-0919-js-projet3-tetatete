8f8cd9f52f5383a99688ff810017a2943d7c3530
valentin <valentin.faucon@gmail.com>
2019-12-10 11:52:05 +0100

    InfoProfilepage component done

--- frontend/src/components/InfoProfilePage.jsx
+++ frontend/src/components/InfoProfilePage.jsx
@@ -3,26 +3,33 @@ import "./styles/InfoProfilePage.scss";
 
 function InfoProfilePage() {
   return (
-    <div className='"infoProfilepage'>
+    <div className="infoProfilePage">
       <img
         classname="profileImage"
-        src="https://via.placeholder.com/300x300"
+        src="https://via.placeholder.com/600x600"
         alt="..."
       />
-      <div>
-        <h2>Lorem ipsum</h2>
-        <img
-          className="profileTag"
-          src="https://via.placeholder.com/50x50"
-          alt="..."
-        />
+
+      <div className="infoContainer">
+        <div className="firstContainer">
+          <h2>Lorem ipsum</h2>
+          <div className="profileTag">
+            <img src="https://via.placeholder.com/50x50" alt="..." />
+            <img src="https://via.placeholder.com/50x50" alt="..." />
+            <img src="https://via.placeholder.com/50x50" alt="..." />
+          </div>
+        </div>
+
+        <p className="resumeInfoPage">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,
+          recusandae. Totam laboriosam, odio eveniet minima magnam deleniti!
+          Neque explicabo corporis, quidem mollitia optio excepturi aspernatur
+          non autem laudantium error sunt.Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Dolore, recusandae. Totam laboriosam,
+          odio eveniet minima magnam deleniti! Neque explicabo corporis, quidem
+          mollitia optio excepturi aspernatur non autem laudantium error sunt.
+        </p>
       </div>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,
-        recusandae. Totam laboriosam, odio eveniet minima magnam deleniti! Neque
-        explicabo corporis, quidem mollitia optio excepturi aspernatur non autem
-        laudantium error sunt.
-      </p>
     </div>
   );
 }
