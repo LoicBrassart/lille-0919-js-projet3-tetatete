61b4fac66a391fcf4bfd434da7c4b333b89a301e
Saulnier <bastien.saul@gmail.com>
2019-12-10 21:00:32 +0100

    Imported components + switch and routes for app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Meetings from "./pages/Meetings";
+import Ambassadors from "./pages/Ambassadors.jsx";
+import Causes from "./pages/Causes";
+import Blog from "./pages/Blog";
+import Footer from "./components/Footer";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 const { siteTitle } = require("./conf.js");
@@ -6,7 +14,19 @@ const { siteTitle } = require("./conf.js");
 document.title = siteTitle;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/meetings" component={Meetings} />
+        <Route path="/ambassadors" component={Ambassadors} />
+        <Route path="/causes" component={Causes} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
