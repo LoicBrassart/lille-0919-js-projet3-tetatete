d78390ba9a36f61c1dde647dabf35cd3c6e34036
Saulnier <bastien.saul@gmail.com>
2019-12-10 21:04:30 +0100

    Home component + concerned css

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../components/Hero";
+import TagLine from "../components/TagLine";
+import LearnMore from "../components/LearnMore";
+import CampaignCard from "../components/CampaignCard";
+import "./styles/Home.scss";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Hero />
+      <TagLine />
+      <div className="HomeContainer">
+        <CampaignCard />
+        <CampaignCard />
+        <CampaignCard />
+        <CampaignCard />
+      </div>
+      <LearnMore />
+    </div>
+  );
+}
+
+export default Home;
