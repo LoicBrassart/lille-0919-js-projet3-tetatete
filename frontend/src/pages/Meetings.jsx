58842a66b92b88f7c636a02e8a35969550063784
Saulnier <bastien.saul@gmail.com>
2019-12-10 21:02:22 +0100

    Meetings page + conerned content and css

---
+++ frontend/src/pages/Meetings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Meetings.scss";
+
+function Meetings() {
+  return (
+    <div className="Meetings">
+      <div className="MeetingsContainer"></div>
+    </div>
+  );
+}
+
+export default Meetings;
