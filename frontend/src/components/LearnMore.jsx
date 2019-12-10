c8023754a83dc6b773843a260f563fb0ea662543
Saulnier <bastien.saul@gmail.com>
2019-12-10 21:04:09 +0100

    Learn more component + concerned css

---
+++ frontend/src/components/LearnMore.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./styles/LearnMore.scss";
+
+export default function LearnMore() {
+  return (
+    <div class="LearnMore">
+      <hr></hr>
+      <section id="article">
+        <article className="box">
+          <p id="title">On parle de nous</p>
+          <hr></hr>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p
+            id="readMore" //onClick TODO
+          >
+            En savoir plus...
+          </p>
+        </article>
+        <article className="box">
+          <p id="title">Notre charte éthique</p>
+          <hr></hr>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p
+            id="readMore" //onClick TODO
+          >
+            En savoir plus...
+          </p>
+        </article>
+        <article className="box">
+          <p id="title">Les dons collectés</p>
+          <hr></hr>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p
+            id="readMore" //onClick TODO
+          >
+            En savoir plus...
+          </p>
+        </article>
+      </section>
+    </div>
+  );
+}
