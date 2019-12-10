317f949726524d6d8ef2af8a0e92b3e6b2368a48
Saulnier <bastien.saul@gmail.com>
2019-12-10 21:01:49 +0100

    Blog page + conerned content and css

---
+++ frontend/src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Blog.scss";
+
+function Blog() {
+  return (
+    <div className="Blog">
+      <div className="BlogContainer"></div>
+    </div>
+  );
+}
+
+export default Blog;
