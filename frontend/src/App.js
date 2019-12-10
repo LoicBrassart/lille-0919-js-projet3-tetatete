57cadf24ee52ea051daf3de3bb89dae362e61408
valentin <valentin.faucon@gmail.com>
2019-12-10 16:54:45 +0100

    add some css modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.scss";
-import InfoprofilePage from './components/InfoProfilePage';
 
 const { siteTitle } = require("./conf.js");
 
 function App() {
   return <div className="App">
-    <InfoprofilePage />
   </div>;
 }
 
