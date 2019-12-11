f620bc7471b8ee720bdfbe645034f5bb64881973
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-12-11 18:15:23 +0100

    change structure and classes names

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,24 +3,45 @@ import "./styles/Footer.scss";
 
 function Footer() {
   return (
-    <div className="Footer">
-      <div className="leftContainer">
-        <img src="" alt="logo site" />
-      </div>
-      <div className="midContainer">
-        <p>Comment ça marche</p>
-        <p>La charte étique</p>
-        <p>Les mentions légales et CGU</p>
-      </div>
-      <div className="rightContainer">
-        <p>Rejoigner-nous sur :</p>
-        <div className="logoContainer">
-          <img src="img/fcb_logo.svg" alt="Facebook" />
-          <img src="img/twitter_logo.svg" alt="Twitter" />
-          <img src="img/insta_logo.svg" alt="Instagram" />
-        </div>
-      </div>
-    </div>
+    <section className="Footer">
+      <ul className="footerContainer">
+        <li className="footerInfos">
+          <ul>
+            <li>Comment ça marche ?</li>
+            <li>La charte étique</li>
+            <li>Les mentions légales et CGU</li>
+          </ul>
+        </li>
+        <li className="footerLogo">
+          <a href="https://www.facebook.com/">
+            <img
+              src="https://via.placeholder.com/300x100?text=Logo+and+name+of+site"
+              alt="site icon"
+            />
+          </a>
+        </li>
+        <li className="footerSocial">
+          <p>Rejoigner-nous sur :</p>
+          <ul>
+            <li>
+              <a>
+                <img src="img/fcb_logo.svg" alt="Facebook" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="img/twitter_logo.svg" alt="Twitter" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="img/insta_logo.svg" alt="Instagram" />
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </section>
   );
 }
 
