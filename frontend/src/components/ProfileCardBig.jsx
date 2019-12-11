979d292221a3e14b9ecb9e0aa97df67b29779306
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-11 11:28:36 +0100

    creation new component with only one placeholder

---
+++ frontend/src/components/ProfileCardBig.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProfileCardBig() {
+  return (
+    <div>
+      <img
+        classname="profileImage"
+        src="https://via.placeholder.com/500x500"
+        alt="..."
+      />
+    </div>
+  );
+}
+
+export default ProfileCardBig;
