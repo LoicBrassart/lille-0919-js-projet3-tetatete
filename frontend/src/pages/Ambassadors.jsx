b33d238050865a309838be219bf9e1149fe23675
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-12-11 09:53:10 +0100

    add generic card in component

--- frontend/src/pages/Ambassadors.jsx
+++ frontend/src/pages/Ambassadors.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FilterTab from "../components/FilterTab";
 import "./styles/Ambassadors.scss";
+import ProfileCard from "../components/ProfileCard";
 
 function Ambassadors() {
   return (
     <div className="Ambassadors">
       <FilterTab filterType="Ambassadeurs" />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
       <div className="AmbassadorsContainer"></div>
     </div>
   );
