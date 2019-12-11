f81ec7135d7da2b621e3fd91e6632e1c7b467620
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-12-11 09:53:23 +0100

    add generic card in component

--- frontend/src/pages/Causes.jsx
+++ frontend/src/pages/Causes.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FilterTab from "../components/FilterTab";
 import "./styles/Causes.scss";
+import ProfileCard from "../components/ProfileCard";
 
 function Causes() {
   return (
     <div className="Causes">
       <FilterTab filterType="Causes" />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
       <div className="CausesContainer"></div>
     </div>
   );
