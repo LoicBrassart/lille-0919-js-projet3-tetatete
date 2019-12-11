95661f1cb6c7011fb850c4fba602858231e7261a
Saulnier <bastien.saul@gmail.com>
2019-12-11 12:03:12 +0100

    made max before meeting

--- frontend/src/pages/Meetings.jsx
+++ frontend/src/pages/Meetings.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import CampaignCard from "../components/CampaignCard";
 import "./styles/Meetings.scss";
 
 function Meetings() {
   return (
     <div className="Meetings">
-      <div className="MeetingsContainer"></div>
+      <div className="MeetingsContainer center">
+        <div className="toggler">
+          <div className="desc">
+            <strong>Am Stram Gram...</strong>
+            <p>Choisis qui tu veux rencontrer !</p>
+          </div>
+          <div className="toggle">
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+            <div className="currentFilter">
+              <p>Rencontres encore en jeu</p>
+              <p>Rencontres passées</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="cards">
+          <CampaignCard />
+          <CampaignCard />
+          <CampaignCard />
+          <CampaignCard />
+          <CampaignCard />
+          <CampaignCard />
+        </div>
+      </div>
     </div>
   );
 }
