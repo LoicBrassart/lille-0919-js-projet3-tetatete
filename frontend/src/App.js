0af6b2e4c08e457ae4dea61e22352d3ef76dd2e3
Theo Boschetti <boschetti.theo@gmail.com>
2019-12-11 12:19:56 +0100

    commit merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,8 @@ import Causes from "./pages/Causes";
 import Blog from "./pages/Blog";
 import CausePage from "./pages/CausePage";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import Campaign from './pages/Campaign';
-=======
 import AmbassadorPage from "./pages/AmbassadorPage";
->>>>>>> 32ed425e2eea2fdaf447a5b57c4bd76039a6b08c
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
@@ -29,11 +26,8 @@ function App() {
         <Route path="/ambassadors" component={Ambassadors} />
         <Route path="/causes" component={Causes} />
         <Route path="/blog" component={Blog} />
-<<<<<<< HEAD
         <Route path="/campaign/:id" component={Campaign} />
-=======
         <Route path="/causepage" component={CausePage} />
->>>>>>> 32ed425e2eea2fdaf447a5b57c4bd76039a6b08c
       </Switch>
       <Footer />
     </div>
