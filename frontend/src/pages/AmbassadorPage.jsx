fac605c908ef17378ef9e5754321581786490642
Saulnier <bastien.saul@gmail.com>
2019-12-11 14:28:43 +0100

    urgence

--- frontend/src/pages/AmbassadorPage.jsx
+++ frontend/src/pages/AmbassadorPage.jsx
@@ -6,46 +6,41 @@ import "./styles/AmbassadorPage.scss";
 function AmbassadorPage() {
   return (
     <div className="AmbassadorPage center">
-      <div className="picAmbassador">
-        <ProfileCardBig classname="picture" />
-        <div id="ambassadorText">
-          <h3>Ambassadeur #01</h3>
-          <ul>
-            <li>
-              {" "}
-              <img
-                className="profileTag"
-                src="https://via.placeholder.com/50x50"
-                alt="..."
-              />
-            </li>
-            <li>
-              {" "}
-              <img
-                className="profileTag"
-                src="https://via.placeholder.com/50x50"
-                alt="..."
-              />
-            </li>
-            <li>
-              {" "}
-              <img
-                className="profileTag"
-                src="https://via.placeholder.com/50x50"
-                alt="..."
-              />
-            </li>
-          </ul>
+      <div className="causepage">
+        <ProfileCardBig />
+        <div className="elementscontainer">
+          <div className="titleandlogo">
+            <h3>Ambasadeur #01</h3>
+            <ul>
+              <li>
+                <img
+                  className="profileTag"
+                  src="https://via.placeholder.com/50x50"
+                  alt="..."
+                />
+              </li>
+              <li>
+                <img
+                  className="profileTag"
+                  src="https://via.placeholder.com/50x50"
+                  alt="..."
+                />
+              </li>
+              <li>
+                <img
+                  className="profileTag"
+                  src="https://via.placeholder.com/50x50"
+                  alt="..."
+                />
+              </li>
+            </ul>
+          </div>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            fugit, commodi eaque quod voluptatem labore, et, sapiente nisi
+            inventore qui velit laboriosam expedita quasi veniam impedit vel
+            tenetur ipsam earum.
           </p>
         </div>
       </div>
