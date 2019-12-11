59e1968190756eaeefe0b3648fe8e53c86eb9a6e
Alice Thammavong <thammavongalice@gmail.com>
2019-12-11 17:11:40 +0100

    Change id to className

--- frontend/src/components/LearnMore.jsx
+++ frontend/src/components/LearnMore.jsx
@@ -3,43 +3,43 @@ import "./styles/LearnMore.scss";
 
 export default function LearnMore() {
   return (
-    <div class="LearnMore">
+    <div className="LearnMore">
       <section id="article">
         <article className="box">
-          <p id="title">On parle de nous</p>
+          <p className="title">On parle de nous</p>
           <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <p
-            id="readMore" //onClick TODO
+            className="readMore" //onClick TODO
           >
             En savoir plus...
           </p>
         </article>
         <article className="box">
-          <p id="title">Notre charte éthique</p>
+          <p className="title">Notre charte éthique</p>
           <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <p
-            id="readMore" //onClick TODO
+            className="readMore" //onClick TODO
           >
             En savoir plus...
           </p>
         </article>
         <article className="box">
-          <p id="title">Les dons collectés</p>
+          <p className="title">Les dons collectés</p>
           <hr></hr>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <p
-            id="readMore" //onClick TODO
+            className="readMore" //onClick TODO
           >
             En savoir plus...
           </p>
