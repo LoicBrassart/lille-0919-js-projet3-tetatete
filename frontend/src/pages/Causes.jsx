a291cd126ccb365dff173bf1ed66a07d889cf894
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-11 11:27:48 +0100

    Import Link

--- frontend/src/pages/Causes.jsx
+++ frontend/src/pages/Causes.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import FilterTab from "../components/FilterTab";
 import "./styles/Causes.scss";
 import ProfileCard from "../components/ProfileCard";
+import { Link } from "react-router-dom";
 
 function Causes() {
   return (
     <div className="Causes">
       <FilterTab filterType="Causes" />
+      <Link to="/causepage">
+        <ProfileCard />
+      </Link>
+
       <ProfileCard />
       <ProfileCard />
       <ProfileCard />
       <ProfileCard />
       <ProfileCard />
-      <ProfileCard />
+
       <div className="CausesContainer"></div>
     </div>
   );
