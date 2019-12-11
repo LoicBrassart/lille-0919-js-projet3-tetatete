fc5e8ce60d9db752d34c626f9a24a5ee7f0d0e9e
Alice Thammavong <thammavongalice@gmail.com>
2019-12-11 11:58:25 +0100

    Import page scss

--- frontend/src/pages/AmbassadorPage.jsx
+++ frontend/src/pages/AmbassadorPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import CampaignCardSmall from "../components/CampaignCardSmall";
+import "./styles/AmbassadorPage.scss";
 
 function AmbassadorPage() {
   return (
-    <div className="AmbassadorPage">
-      <div>
-        <p>
+    <div className="ambassadorPage">
+      <div className="picAmbassador">
+        <div>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+          aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
       </div>
       <p>Campagnes de Ambassadeur #01:</p>
       <div className="bottomCards">
