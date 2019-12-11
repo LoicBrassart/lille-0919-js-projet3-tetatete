71ea1f2fa7a0f76b990d308ed04b69608ba4bffa
Alice Thammavong <thammavongalice@gmail.com>
2019-12-11 11:09:45 +0100

    Add CampaignCardSmall to PageAmbassador

--- frontend/src/pages/AmbassadorPage.jsx
+++ frontend/src/pages/AmbassadorPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import CampaignCardSmall from "../components/CampaignCardSmall";
 
 function PageAmbassador() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <p>Campagnes de Ambassadeur #01:</p>
+      <div className="bottomCards">
+        <CampaignCardSmall />
+        <CampaignCardSmall />
+        <CampaignCardSmall />
+      </div>
+    </div>
+  );
 }
 
 export default PageAmbassador;
