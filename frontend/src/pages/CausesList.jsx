a299f1bd4e65e488711f8588d9f05a4f1d10fa97
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-12 17:17:43 +0100

    just create the basci function CausesList

---
+++ frontend/src/pages/CausesList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FilterTab from "../components/FilterTab";
+import ProfileCard from "../components/Profilecard";
+import { Link } from "react-router-dom";
+import "./styles/CausesList.scss";
+
+function CausesList() {
+  return <div>liste des causes</div>;
+}
+
+export default CausesList;
