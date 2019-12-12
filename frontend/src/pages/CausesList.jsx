6412bd0f6a18b2adbd75976e78fb6a25d658d2ba
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-12 17:25:05 +0100

    bring the code from my predev FilterTab ProfileCar

--- frontend/src/pages/CausesList.jsx
+++ frontend/src/pages/CausesList.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import FilterTab from "../components/FilterTab";
-import ProfileCard from "../components/Profilecard";
+import ProfileCard from "../components/ProfileCard";
 import { Link } from "react-router-dom";
-import "./styles/CausesList.scss";
+import "./styles/Causes.scss";
 
-function CausesList() {
-  return <div>liste des causes</div>;
+function Causes() {
+  return (
+    <div className="causes">
+      <FilterTab filterType="Causes" />
+      <div className="causeContainer">
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+        <Link to="/causepage">
+          <ProfileCard />
+        </Link>
+      </div>
+
+      <div className="CausesContainer"></div>
+    </div>
+  );
 }
 
-export default CausesList;
+export default Causes;
