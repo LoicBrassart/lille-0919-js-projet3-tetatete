dc2a5a5f183464b39c31adeaa7f7fb485e218565
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-12 17:17:05 +0100

    Bring component and Route on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import CausesList from "./pages/CausesList";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 const { siteTitle } = require("./conf.js");
 
 function App() {
-  return <div className="App">
-  </div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/causeslist" component={CausesList} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
