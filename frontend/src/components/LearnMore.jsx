ba2b056b5ae116bb7c0608e47093d3ab3c435905
Alice Thammavong <thammavongalice@gmail.com>
2019-12-12 11:05:27 +0100

    Change text into pictures

--- frontend/src/components/LearnMore.jsx
+++ frontend/src/components/LearnMore.jsx
@@ -3,17 +3,60 @@ import "./styles/LearnMore.scss";
 
 export default function LearnMore() {
   return (
-    <div className="LearnMore">
-      <section id="article ">
-        <article className="box">
-          <img src="https://via.placeholder.com/300x350" alt="..." />
-        </article>
-        <article className="box">
-          <img src="https://via.placeholder.com/300x350" alt="..." />
-        </article>
-        <article className="box">
-          <img src="https://via.placeholder.com/300x350" alt="..." />
-        </article>
+    <div className="LearnMore center">
+      <section id="article">
+        <figure className="img">
+          <img src="https://via.placeholder.com/300x350" alt="..."></img>
+          <figcaption>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
+            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
+            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
+            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
+            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
+            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
+            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
+            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
+            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
+            pretium sit amet, tempor quis, urna.
+          </figcaption>
+        </figure>
+
+        <figure className="img">
+          <img src="https://via.placeholder.com/300x350" alt="..."></img>
+          <figcaption>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
+            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
+            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
+            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
+            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
+            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
+            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
+            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
+            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
+            pretium sit amet, tempor quis, urna.
+          </figcaption>
+        </figure>
+        <figure className="img">
+          <img src="https://via.placeholder.com/300x350" alt="..."></img>
+          <figcaption>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
+            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
+            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
+            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
+            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
+            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
+            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
+            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
+            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
+            pretium sit amet, tempor quis, urna.
+          </figcaption>
+        </figure>
       </section>
     </div>
   );
