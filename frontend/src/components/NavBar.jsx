250c894c88a8ac6c1851a79e069f590ce1585b2f
Saulnier <bastien.saul@gmail.com>
2019-12-12 11:59:19 +0100

    Made navbar component

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { scaleRotate as BurgerMenu } from "react-burger-menu";
+import { NavLink } from "react-router-dom";
+import "./styles/NavBar.scss";
+
+class NavBar extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+      <header className="NavBar">
+        <nav className="desktopNav">
+          <NavLink className="logo" activeClassName="active" exact to="/">
+            TooTasty
+            <img src="" alt="" />
+          </NavLink>
+
+          <ul className="desktopMenu">
+            <li>
+              <NavLink
+                id="meetings"
+                className="menu-item"
+                activeClassName="active"
+                to="/meetings"
+              >
+                Rencontres
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                id="meetings"
+                className="menu-item"
+                activeClassName="active"
+                to="/ambassadors"
+              >
+                Ambassadors
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                id="meetings"
+                className="menu-item"
+                activeClassName="active"
+                to="/causes"
+              >
+                Causes soutenues
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                id="meetings"
+                className="menu-item"
+                activeClassName="active"
+                to="/blog"
+              >
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <BurgerMenu width={320}>
+          <NavLink
+            id="meetings"
+            className="menu-item"
+            activeClassName="bm-active"
+            to="/meetings"
+          >
+            Rencontres
+          </NavLink>
+
+          <NavLink
+            id="meetings"
+            className="menu-item"
+            activeClassName="bm-active"
+            to="/ambassadors"
+          >
+            Ambassadors
+          </NavLink>
+
+          <NavLink
+            id="meetings"
+            className="menu-item"
+            activeClassName="bm-active"
+            to="/causes"
+          >
+            Causes soutenues
+          </NavLink>
+
+          <NavLink
+            id="meetings"
+            className="menu-item"
+            activeClassName="bm-active"
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </BurgerMenu>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
