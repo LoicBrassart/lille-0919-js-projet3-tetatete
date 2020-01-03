af592850bd26d7557cb366d412395b3a52156678
Theo Boschetti <boschetti.theo@gmail.com>
2020-01-03 17:07:41 +0100

    association routes created

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const { port } = require('./conf');
 
 const ambassador = require('./routes/ambassador');
+const association = require('./routes/association');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/ambassador', ambassador);
+app.use('/association', association);
 
 app.listen(port, err => {
     if (err) {
