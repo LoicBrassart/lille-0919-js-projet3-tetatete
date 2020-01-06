c40201263c215b3fd9ac3a4239f5e5656a297098
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-06 10:04:06 +0100

    adding all components in page

--- frontend/src/pages/Ambassadors.jsx
+++ frontend/src/pages/Ambassadors.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Profiler } from "react";
+import "./styles/Ambassadors.scss";
+import FilterTab from "../components/FilterTab";
+import ProfileCard from "../components/ProfileCard";
 
 function Ambassadors() {
-  return <section className="Ambassadors"></section>;
+  return (
+    <section className="Ambassadors">
+      <FilterTab />
+      <div className="center containerProfile">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+      </div>
+    </section>
+  );
 }
 
 export default Ambassadors;
