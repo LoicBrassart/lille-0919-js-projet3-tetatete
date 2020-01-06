e9de496b3b6ab20fd13a6c142b84c12193d7cc18
Saulnier <bastien.saul@gmail.com>
2020-01-06 11:30:10 +0100

    made jsx squeletton for meetings toggler

--- frontend/src/pages/Meetings.jsx
+++ frontend/src/pages/Meetings.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Meetings() {
-  return <section className="Meetings"></section>;
+  return (
+    <section className="Meetings">
+      <div className="toggler">
+        <div className="desc">
+          <strong>Am Stram Gram...</strong>
+          <p>Choisis qui tu veux rencontrer !</p>
+        </div>
+        <div className="toggle">
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <div className="currentFilter">
+            <p>Rencontres encore en jeu</p>
+            <p>Rencontres passées</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Meetings;
