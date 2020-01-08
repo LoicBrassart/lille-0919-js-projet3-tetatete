fa960d209b03c4d6d75d78ed25cf4a416b200fe8
Saulnier <bastien.saul@gmail.com>
2020-01-08 16:06:36 +0100

    Made CauseInfos component ( axios call + props dispatch )

---
+++ frontend/src/pages/CauseInfos.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles/CauseInfos.scss";
+
+function CauseInfos(props) {
+  const [association, setAssociation] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/association/${props.match.params.id}`)
+      .then(res => {
+        setAssociation(res.data[0]);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [setAssociation]);
+
+  return (
+    <div className="CauseInfos center">
+      <div className="photo">
+        <img src={association.img} alt="" className="associationPhoto" />
+      </div>
+      <div className="content">
+        <div className="titleAndTags">
+          <h3>{association.name}</h3>
+          <ul>
+            <li>
+              <img
+                className="profileTag"
+                src="https://via.placeholder.com/50x50"
+                alt="..."
+              />
+            </li>
+            <li>
+              <img
+                className="profileTag"
+                src="https://via.placeholder.com/50x50"
+                alt="..."
+              />
+            </li>
+            <li>
+              <img
+                className="profileTag"
+                src="https://via.placeholder.com/50x50"
+                alt="..."
+              />
+            </li>
+          </ul>
+        </div>
+
+        <p>{association.resume}</p>
+        <a href={association.website} target="blank">
+          SITE WEB
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default CauseInfos;
