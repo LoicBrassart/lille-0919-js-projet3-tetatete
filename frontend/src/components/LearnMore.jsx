1a38aee7609c1d8eef3610bc26427e31a8ffa28b
Alice Thammavong <thammavongalice@gmail.com>
2020-01-08 17:57:36 +0100

    Install react tooltip+set tooltip

--- frontend/src/components/LearnMore.jsx
+++ frontend/src/components/LearnMore.jsx
@@ -1,63 +1,51 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 import "./styles/LearnMore.scss";
 
 export default function LearnMore() {
   return (
     <div className="LearnMore center">
-      <section id="article">
-        <figure className="img">
+      <ul className="list">
+        <li data-tip data-for="press" data-class="press">
           <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
+        </li>
+        <li data-tip data-for="cause">
+          <img src="https://via.placeholder.com/300x350" alt="..."></img>
+        </li>
+        <li data-tip data-for="donation">
+          <img src="https://via.placeholder.com/300x350" alt="..."></img>
+        </li>
 
-        <figure className="img">
-          <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
+        <ReactTooltip id="press" place="left" type="info">
+          <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
-        <figure className="img">
-          <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </div>
+        </ReactTooltip>
+        <ReactTooltip id="cause" place="bottom" type="info">
+          <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
-      </section>
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.{" "}
+          </div>
+        </ReactTooltip>
+        <ReactTooltip id="donation" place="right" type="info">
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{" "}
+          </div>
+        </ReactTooltip>
+      </ul>
     </div>
   );
 }
