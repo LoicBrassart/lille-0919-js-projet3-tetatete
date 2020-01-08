411c13e13be8a26aff4100f58d76d51fe7f31945
Saulnier <bastien.saul@gmail.com>
2020-01-08 10:38:03 +0100

    made axios import with hooks and .map on associations with Profile Card return

--- frontend/src/pages/Causes.jsx
+++ frontend/src/pages/Causes.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Causes.scss";
 import FilterTab from "../components/FilterTab";
 import ProfileCard from "../components/ProfileCard";
+import axios from "axios";
 
 function Causes() {
+  const [associations, setAssociations] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/association`)
+      .then(res => {
+        setAssociations(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [setAssociations]);
   return (
     <section className="Causes">
       <FilterTab />
       <div className="center containerCauses">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+        {associations.map(association => {
+          return (
+            <ProfileCard
+              id={association.id}
+              key={association.id}
+              img={association.img}
+              name={association.name}
+            />
+          );
+        })}
       </div>
     </section>
   );
