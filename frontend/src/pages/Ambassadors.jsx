64754aea08fc1eda8f364a832f569314de7bcdb9
Philippe Lambet <philippe.lambert.fr@gmail.com>
2020-01-08 16:58:54 +0100

    made axios call and map on profileCard with correct props

--- frontend/src/pages/Ambassadors.jsx
+++ frontend/src/pages/Ambassadors.jsx
@@ -1,19 +1,39 @@
-import React, { Profiler } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Ambassadors.scss";
 import FilterTab from "../components/FilterTab";
 import ProfileCard from "../components/ProfileCard";
+import axios from "axios";
 
 function Ambassadors() {
+  const [ambassadors, setAmbassadors] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/ambassador`)
+      .then(res => {
+        setAmbassadors(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [setAmbassadors]);
+
   return (
     <section className="Ambassadors">
       <FilterTab />
       <div className="center containerProfile">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+        {ambassadors.map(ambassador => {
+          return (
+            <ProfileCard
+              key={ambassador.id}
+              id={ambassador.id}
+              img={ambassador.img}
+              url="ambassadors"
+              name={`${ambassador.firstname} ${ambassador.lastname}`}
+            />
+          );
+        })}
       </div>
     </section>
   );
