7082cc419d8a8076d6f6a5a41f9fba69b129338b
Saulnier <bastien.saul@gmail.com>
2020-01-08 10:36:45 +0100

    made get route for association table

--- backend/routes/association.js
+++ backend/routes/association.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 //Get all associations
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM association", (err, results) => {
-        if (err) {
-            res.status(500).send("Error !");
-        } else {
-            res.status(200).json(results);
-        }
-    })
+  connection.query("SELECT * FROM association", (err, results) => {
+    if (err) {
+      res.status(500).send("Error !");
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 module.exports = router;
