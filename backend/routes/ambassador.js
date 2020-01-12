d2972617629993d929733a110bfa92564d7bb17e
Theo Boschetti <boschetti.theo@gmail.com>
2020-01-12 15:29:03 +0100

    added post for new ambassador or new association and get by id

--- backend/routes/ambassador.js
+++ backend/routes/ambassador.js
@@ -2,9 +2,9 @@ const { connection } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-//Get all ambassadors
+//Get all ambassadors order by name
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM ambassador", (err, results) => {
+    connection.query("SELECT * FROM ambassador ORDER BY name ASC", (err, results) => {
         if (err) {
             res.status(500).send("Error !");
         } else {
@@ -12,5 +12,26 @@ router.get("/", (req, res) => {
         }
     })
 });
+
+//Get ambassador by id
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM ambassador WHERE id = ?", [id], (err, results) => {
+        if (err) {
+            res.status(500).send("Error !");
+        } else {
+            res.status(200).json(results);
+        }
+    })
+});
+
+//Post a new ambassador
+router.post("/new", (req, res) => {
+    const data = req.body;
+    connection.query("INSERT INTO ambassador SET ?", [data], (err, results) => {
+        if (err) return res.status(500).send("Error !");
+        return res.sendStatus(200);
+    })
+})
 
 module.exports = router;
--- backend/routes/association.js
+++ backend/routes/association.js
@@ -2,9 +2,9 @@ const { connection } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-//Get all associations
+//Get all associations order by name
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM association", (err, results) => {
+    connection.query("SELECT * FROM association ORDER BY name ASC", (err, results) => {
         if (err) {
             res.status(500).send("Error !");
         } else {
@@ -12,5 +12,26 @@ router.get("/", (req, res) => {
         }
     })
 });
+
+//Get association by id
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    connection.query("SELECT * FROM association WHERE id = ?", [id], (err, results) => {
+        if (err) {
+            res.status(500).send("Error !");
+        } else {
+            res.status(200).json(results);
+        }
+    })
+});
+
+//Post a new association
+router.post("/new", (req, res) => {
+    const data = req.body;
+    connection.query("INSERT INTO association SET ?", [data], (err, results) => {
+        if (err) return res.status(500).send("Error !");
+        return res.sendStatus(200);
+    })
+})
 
 module.exports = router;
