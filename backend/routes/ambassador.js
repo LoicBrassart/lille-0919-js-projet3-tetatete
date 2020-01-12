35945a2374530eea9a82578ce26e7015b71c4d3e
Theo Boschetti <boschetti.theo@gmail.com>
2020-01-12 21:44:57 +0100

    filter added to get routes of ambassador and association

--- backend/routes/ambassador.js
+++ backend/routes/ambassador.js
@@ -2,14 +2,21 @@ const { connection } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-//Get all ambassadors order by name
+//Get all ambassadors order by name and with filter
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM ambassador ORDER BY name ASC", (err, results) => {
-        if (err) {
-            res.status(500).send("Error !");
-        } else {
-            res.status(200).json(results);
-        }
+    let sql = "SELECT * FROM ambassador ORDER BY firstname ASC";
+    let filter = [];
+    if (req.query.filter) {
+        sql = `SELECT * FROM ambassador 
+                JOIN ambassador_has_tag ON ambassador.id = ambassador_has_tag.id_ambassador
+                WHERE id_tag = ?
+                ORDER BY firstname ASC`;
+        filter.push(req.query.filter);
+    }
+    connection.query(sql, [filter], (err, results) => {
+        if (err) return res.status(500).send("Error in obtaining ambassadors's infos !");
+        if (results.length === 0) return res.status(500).send("There is no info corresponding to your research.");
+        return res.status(200).json(results);
     })
 });
 
@@ -17,11 +24,9 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
     const id = req.params.id;
     connection.query("SELECT * FROM ambassador WHERE id = ?", [id], (err, results) => {
-        if (err) {
-            res.status(500).send("Error !");
-        } else {
-            res.status(200).json(results);
-        }
+        if (err) return res.status(500).send("Error in obtaining ambassador's info !");
+        if (results.length === 0) return res.status(500).send("There is no info corresponding to your research.");
+        return res.status(200).json(results);
     })
 });
 
@@ -29,7 +34,7 @@ router.get("/:id", (req, res) => {
 router.post("/new", (req, res) => {
     const data = req.body;
     connection.query("INSERT INTO ambassador SET ?", [data], (err, results) => {
-        if (err) return res.status(500).send("Error !");
+        if (err) return res.status(500).send("Error has occured during the creation of the new ambassador !");
         return res.sendStatus(200);
     })
 })
--- backend/routes/association.js
+++ backend/routes/association.js
@@ -2,26 +2,31 @@ const { connection } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-//Get all associations order by name
+//Get all associations order by name and with filter
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM association ORDER BY name ASC", (err, results) => {
-        if (err) {
-            res.status(500).send("Error !");
-        } else {
-            res.status(200).json(results);
-        }
+    let sql = "SELECT * FROM association ORDER BY name ASC";
+    let filter = [];
+    if (req.query.filter) {
+        sql = `SELECT * FROM association 
+                JOIN association_has_tag ON association.id = association_has_tag.id_association
+                WHERE id_tag = ?
+                ORDER BY name ASC`;
+        filter.push(req.query.filter);
+    }
+    connection.query(sql, [filter], (err, results) => {
+        if (err) return res.status(500).send("Error in obtaining associations's infos !");
+        if (results.length === 0) return res.status(500).send("There is no info corresponding to your research.");
+        return res.status(200).json(results);
     })
-});
+})
 
 //Get association by id
 router.get("/:id", (req, res) => {
     const id = req.params.id;
     connection.query("SELECT * FROM association WHERE id = ?", [id], (err, results) => {
-        if (err) {
-            res.status(500).send("Error !");
-        } else {
-            res.status(200).json(results);
-        }
+        if (err) return res.status(500).send("Error in obtaining association's info !");
+        if (results.length === 0) return res.status(500).send("There is no info corresponding to your research.");
+        return res.status(200).json(results);
     })
 });
 
@@ -29,7 +34,7 @@ router.get("/:id", (req, res) => {
 router.post("/new", (req, res) => {
     const data = req.body;
     connection.query("INSERT INTO association SET ?", [data], (err, results) => {
-        if (err) return res.status(500).send("Error !");
+        if (err) return res.status(500).send("Error has occured during the creation of the new association !");
         return res.sendStatus(200);
     })
 })
