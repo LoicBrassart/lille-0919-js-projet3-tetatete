6ee438f966816fff4cf9c11338cf0fe97805c746
Saulnier <bastien.saul@gmail.com>
2020-01-13 13:57:42 +0100

    made learnmoreCard component + respective css

--- frontend/src/components/LearnMore.jsx
+++ frontend/src/components/LearnMore.jsx
@@ -1,62 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/LearnMore.scss";
 
 export default function LearnMore() {
+  const [showMeex, setShowMeex] = useState(false);
+  const [showChart, setShowChart] = useState(false);
+  const [showFounds, setShowFounds] = useState(false);
   return (
-    <div className="LearnMore center">
-      <section id="article">
-        <figure className="img">
-          <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
+    <div className="LearnMore">
+      <section>
+        <article className="learnCard" onClick={() => setShowMeex(!showMeex)}>
+          <span className={showMeex ? "shown" : ""}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ea
+              perspiciatis excepturi velit, quaerat soluta accusamus enim fuga
+              molestiae aperiam nostrum voluptatibus aspernatur, assumenda nobis
+              facere, voluptatem iure totam nesciunt.
+            </p>
+          </span>
+          <figure>
+            <img src="img/learn-meex.jpg" alt="learn-meex" />
+          </figure>
+          <div className="content">
+            <div className="titleBorder"></div>
+            <div className="titleLayout">
+              <h3>MEEX ?</h3>
+            </div>
+          </div>
+        </article>
 
-        <figure className="img">
-          <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
-        <figure className="img">
-          <img src="https://via.placeholder.com/300x350" alt="..."></img>
-          <figcaption>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo
-            accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper
-            ultricies, lacus quam ultricies odio, vitae placerat pede sem sit
-            amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui
-            leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel
-            massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque
-            libero. Maecenas libero.Etiam dictum tincidunt diam. Donec ipsum
-            massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse
-            nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec,
-            pretium sit amet, tempor quis, urna.
-          </figcaption>
-        </figure>
+        <article className="learnCard" onClick={() => setShowChart(!showChart)}>
+          <span className={showChart ? "shown" : ""}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ea
+              perspiciatis excepturi velit, quaerat soluta accusamus enim fuga
+              molestiae aperiam nostrum voluptatibus aspernatur, assumenda nobis
+              facere, voluptatem iure totam nesciunt.
+            </p>
+          </span>
+          <figure>
+            <img src="img/learn-chart.jpg" alt="learn-meex" />
+          </figure>
+          <div className="content">
+            <div className="titleBorder"></div>
+            <div className="titleLayout">
+              <h3>
+                Charte
+                <br />
+                éthique
+              </h3>
+            </div>
+          </div>
+        </article>
+
+        <article
+          className="learnCard"
+          onClick={() => setShowFounds(!showFounds)}
+        >
+          <span className={showFounds ? "shown" : ""}>
+            <p>meeX, c'est :</p>
+            <div className="stats">
+              <big>2 €</big>
+              <small>collectés généreusement</small>
+            </div>
+
+            <big>99</big>
+            <small>associations soutenues</small>
+          </span>
+          <figure>
+            <img src="img/learn-founds.jpg" alt="learn-meex" />
+          </figure>
+          <div className="content">
+            <div className="titleBorder"></div>
+            <div className="titleLayout">
+              <h3>
+                Montants
+                <br />
+                Collectés
+              </h3>
+            </div>
+          </div>
+        </article>
       </section>
     </div>
   );
