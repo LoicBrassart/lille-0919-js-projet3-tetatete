8f3fb03a45296ef6b78ebd7b10c1a5191db57bce
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-14 16:32:27 +0100

    adding

---
+++ frontend/src/pages/FormAdmin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormCreateUser from "../components/FormCreateUser";
+
+export default function FormAdmin() {
+  return (
+    <div className="FormAdmin">
+      <FormCreateUser />
+    </div>
+  );
+}
