127c0af2b2334107a372480247ba1efc17e60edd
Saulnier <bastien.saul@gmail.com>
2020-01-14 14:12:41 +0100

    set up redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,24 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./reset.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./redux/store/index.js";
+
+const createStoreWithMiddleware = compose(applyMiddleware(thunk))(createStore);
+
+const store = createStoreWithMiddleware(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
