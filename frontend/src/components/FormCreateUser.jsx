53c6a4f9b6f6b07cd8bd6e4f9ced4f463ef0c04b
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-14 16:33:30 +0100

    component page and call component form new user

---
+++ frontend/src/components/FormCreateUser.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import "./styles/FormCreateUser.scss";
+import axios from "axios";
+const { apiCall } = require("../conf");
+
+export default function FormCreateUser() {
+  const [user, setUser] = useState({
+    phone_number: "",
+    password: "",
+    email: "",
+    canAdmin: false,
+    canCreateContent: false
+  });
+
+  const resetForm = () => {
+    setUser({
+      phone_number: "",
+      password: "",
+      email: "",
+      canAdmin: false,
+      canCreateContent: false
+    });
+  };
+
+  const handleSubmit = () => {
+    const data = user;
+    console.log(apiCall);
+    axios
+      .post(`${apiCall}/auth`, data)
+      .then(res => {
+        console.log(res.data);
+        console.log("Booyah!");
+        resetForm();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="CampaignFormBO">
+      <h1>Create new user</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <div className="container">
+          <label>Phone number :</label>
+          <input
+            type="tel"
+            value={user.phone_number}
+            onChange={event => {
+              setUser({
+                ...user,
+                phone_number: event.target.value
+              });
+            }}
+            required
+          />
+        </div>
+
+        <div className="container">
+          <label>Password :</label>
+          <input
+            type="text"
+            value={user.password}
+            onChange={event => {
+              setUser({
+                ...user,
+                password: event.target.value
+              });
+            }}
+            required
+          />
+        </div>
+
+        <div className="container">
+          <label>Email :</label>
+          <input
+            type="email"
+            value={user.email}
+            onChange={event => {
+              setUser({
+                ...user,
+                email: event.target.value
+              });
+            }}
+            required
+          />
+        </div>
+
+        <div className="container">
+          <label>Law Admin :</label>
+          <input
+            type="checkbox"
+            value={user.canAdmin}
+            onChange={event => {
+              setUser({
+                ...user,
+                canAdmin: !user.canAdmin
+              });
+            }}
+          />
+        </div>
+
+        <div className="container">
+          <label>Law Create Content :</label>
+          <input
+            type="checkbox"
+            value={user.canCreateContent}
+            onChange={event => {
+              setUser({
+                ...user,
+                canCreateContent: !user.canCreateContent
+              });
+            }}
+          />
+        </div>
+
+        <input type="submit" onClick={handleSubmit} />
+      </form>
+    </div>
+  );
+}
