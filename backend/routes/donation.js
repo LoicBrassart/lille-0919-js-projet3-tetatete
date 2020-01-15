2ca70ca3fe7babbb9bb33010f40ae1529d21a463
Theo Boschetti <boschetti.theo@gmail.com>
2020-01-15 17:29:08 +0100

    Add a fat route for donation

--- backend/routes/donation.js
+++ backend/routes/donation.js
@@ -28,16 +28,69 @@ router.get("/total", (req, res) => {
   );
 });
 
-//Post a donation from a user
+//Post a donation from a new user
 router.post("/", (req, res) => {
-  const donation = req.body;
-  connection.query("INSERT INTO donation SET ?", [donation], (err, results) => {
-    if (err)
-      return res
-        .status(500)
-        .send("Error has occured during the creation of the donation !");
-    return res.status(201).send("Donation posted successfully");
-  });
+  const { phone_number, email, campaign_id, donation_value, date } = req.body;
+  const newUser = {
+    phone_number: phone_number,
+    email: email
+  };
+  connection.query(
+    "SELECT phone_number FROM user WHERE phone_number = ?",
+    [phone_number],
+    (err, results) => {
+      if (err)
+        return res
+          .status(500)
+          .send("Error has occured during the creation of the new user !");
+      if (results.length)
+        return res.status(409).send("Phone number already taken !");
+      connection.query(
+        "SELECT email FROM user WHERE email = ?",
+        [email],
+        (err, results) => {
+          if (err)
+            return res
+              .status(500)
+              .send("Error has occured during the creation of the new user !");
+          if (results.length)
+            return res.status(409).send("Email address already taken !");
+          connection.query(
+            "INSERT INTO user SET ?",
+            [newUser],
+            (err, results) => {
+              if (err)
+                return res
+                  .status(500)
+                  .send(
+                    "Error has occured during the creation of the new user !"
+                  );
+              const { insertId } = results;
+              const newDonation = {
+                campaign_id: campaign_id,
+                user_id: insertId,
+                donation_value: donation_value,
+                date: date
+              };
+              connection.query(
+                "INSERT INTO donation SET ?",
+                [newDonation],
+                (err, results) => {
+                  if (err)
+                    return res
+                      .status(500)
+                      .send(
+                        "Error has occured during the post of the new donation !"
+                      );
+                  return res.status(201).send("Donation posted successfully !");
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 });
 
 module.exports = router;
