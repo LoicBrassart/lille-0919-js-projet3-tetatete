5b89150d83c71c7606a0201895d1663eb2090bcb
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-16 11:21:29 +0100

    adding form for admin login

---
+++ frontend/src/components/FormLoginBO.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import "./styles/FormLoginBO.scss";
+const { apiCall } = require("../conf");
+
+export default function FormLoginBO() {
+  const dispatch = useDispatch();
+  const [credentialsState, updateCredentialsState] = useState(true);
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = () => {
+    axios
+      .post(`${apiCall}/auth/login`, user)
+      .then(res => {
+        dispatch({ type: "JWT", value: res.data });
+      })
+      .catch(err => {
+        updateCredentialsState(false);
+      });
+  };
+
+  return (
+    <div className="FormLoginBO">
+      <h1>Admin login</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        {!credentialsState && (
+          <p className="warning">
+            Veuillez réessayer, l'email ou le mot de passe ne sont incorrect
+          </p>
+        )}
+        <div className="container">
+          <label>Email :</label>
+          <input
+            type="email"
+            value={user.email}
+            onChange={event => {
+              updateCredentialsState(true);
+              setUser({
+                ...user,
+                email: event.target.value
+              });
+            }}
+            className={credentialsState ? "" : "warning"}
+            required
+          />
+        </div>
+
+        <div className="container">
+          <label>Password :</label>
+          <input
+            type="password"
+            value={user.password}
+            onChange={event => {
+              updateCredentialsState(true);
+              setUser({
+                ...user,
+                password: event.target.value
+              });
+            }}
+            className={credentialsState ? "" : "warning"}
+            required
+          />
+        </div>
+        <input type="submit" onClick={handleSubmit} />
+      </form>
+    </div>
+  );
+}
