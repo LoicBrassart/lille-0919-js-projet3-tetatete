a195da169131bcdda689c21ddb1ca7ec3b44df68
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-16 11:20:56 +0100

    adding import redux, react-redux and reducer + call Provider in render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,18 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./reset.scss";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
