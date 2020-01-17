a13dd8c66674b0b654795c523d27c24de21b4eaf
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-17 12:09:32 +0100

    function works

--- frontend/src/components/FormCreateCause.jsx
+++ frontend/src/components/FormCreateCause.jsx
@@ -4,6 +4,14 @@ import axios from "axios";
 const { apiCall } = require("../conf");
 
 export default function FormCreateCause() {
+  const [isChecked, setIsChecked] = useState({
+    box1: false,
+    box2: false,
+    box3: false,
+    box4: false,
+    box5: false,
+    box6: false
+  });
   const [asso, setAsso] = useState({
     name: "",
     img: null,
@@ -17,7 +25,7 @@ export default function FormCreateCause() {
   const handleSubmit = () => {
     console.log(data);
     axios
-      .post(`${apiCall}/association`, data)
+      .post(`${apiCall}/test`, data)
       .then(res => {
         console.log(`Booyah! ${res.data}`);
       })
@@ -102,13 +110,24 @@ export default function FormCreateCause() {
               <input
                 type="checkbox"
                 id="boxSante"
-                name="choix1"
                 value="1"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: event.target.value
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box1
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("1");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxSante">Sante</label>
@@ -116,14 +135,26 @@ export default function FormCreateCause() {
             <div className="box1">
               <input
                 type="checkbox"
-                value={2}
+                value="2"
                 id="boxHumanitaire"
                 name="choix2"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: [event.target.value]
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box2
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("2");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxHumanitaire">Humanitaire</label>
@@ -131,14 +162,26 @@ export default function FormCreateCause() {
             <div className="box1">
               <input
                 type="checkbox"
-                value={3}
+                value="3"
                 id="boxCulture"
                 name="choix3"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: event.target.value
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box3
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("3");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxCulture">Culture</label>
@@ -146,14 +189,26 @@ export default function FormCreateCause() {
             <div className="box1">
               <input
                 type="checkbox"
-                value={4}
+                value="4"
                 id="boxSport"
                 name="choix4"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: event.target.value
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box4
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("4");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxSport">Sport</label>
@@ -161,14 +216,26 @@ export default function FormCreateCause() {
             <div className="box1">
               <input
                 type="checkbox"
-                value={5}
+                value="5"
                 id="boxEcologie"
                 name="choix5"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: event.target.value
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box5
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("5");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxEcologie">Écologie</label>
@@ -176,14 +243,26 @@ export default function FormCreateCause() {
             <div className="box1">
               <input
                 type="checkbox"
-                value={6}
+                value="6"
                 id="boxEducation"
                 name="choix6"
                 onChange={event => {
-                  setAsso({
-                    ...asso,
-                    id_tag: event.target.value
+                  setIsChecked({
+                    ...isChecked,
+                    box1: !isChecked.box6
                   });
+                  const value = event.target.value;
+                  const boxArr = [...asso.id_tag];
+
+                  if (event.target.checked) boxArr.push(value);
+                  else {
+                    const index = boxArr.indexOf("6");
+                    if (index !== -1) {
+                      boxArr.splice(index, 1);
+                    }
+                  }
+                  setAsso({ ...asso, id_tag: boxArr });
+                  console.log(boxArr);
                 }}
               />
               <label htmlFor="boxEducation">Éducation</label>
@@ -195,3 +274,20 @@ export default function FormCreateCause() {
     </div>
   );
 }
+
+/*
+                  const boxArr = [...asso.id_tag];
+                  const value = event.target.value;
+                  const index = boxArr.findIndex(
+                    checkbox => checkbox === value
+                  );
+                  if (index > -1) {
+                    boxArr = [
+                      ...boxArr.slice(0, index),
+                      ...boxArr.slice(index + 1)
+                    ];
+                  } else {
+                    boxArr.push(value);
+                  }
+                  setAsso({ id_tag: boxArr });
+                */
