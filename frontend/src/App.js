822d0cc7b59276eeb9b5e138f81ede0832b60c65
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-17 16:28:25 +0100

    delete road don't need

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,9 @@ import Ambassadors from "./pages/Ambassadors";
 import AmbassadorInfos from "./pages/AmbassadorInfos";
 import Causes from "./pages/Causes";
 import CauseInfos from "./pages/CauseInfos";
-import Blog from "./pages/Blog";
 import Footer from "./components/Footer";
 import "./App.scss";
 import CampaignPage from "./pages/CampaignPage";
-import FormAdmin from "./pages/FormAdmin";
 const { siteTitle } = require("./conf.js");
 
 function App() {
@@ -27,7 +25,6 @@ function App() {
         <Route path="/ambassadors" component={Ambassadors}></Route>
         <Route path="/causes/:id" component={CauseInfos}></Route>
         <Route path="/causes" component={Causes}></Route>
-        <Route path="/admin" component={FormAdmin}></Route>
       </Switch>
       <Footer />
     </div>
