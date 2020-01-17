d9ee42c95d13d687ceede2e04eea5096ef96653a
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-17 15:27:25 +0100

    adding CheckboxList components for .map

---
+++ frontend/src/CheckboxList.js
@@ -0,0 +1,34 @@
+const CheckboxList = [
+  {
+    id: "boxSante",
+    label: "Santé",
+    value: "1"
+  },
+  {
+    id: "boxHumanitaire",
+    label: "Humanitaire",
+    value: "1"
+  },
+  {
+    id: "boxCulture",
+    label: "Culture",
+    value: "1"
+  },
+  {
+    id: "boxSport",
+    label: "Sport",
+    value: "1"
+  },
+  {
+    id: "boxEcologie",
+    label: "Écologie",
+    value: "1"
+  },
+  {
+    id: "boxEducation",
+    label: "Éducation",
+    value: "1"
+  }
+];
+
+export default CheckboxList;
