cee7c2c1f9662ed108054f9f9b58042a52ee176a
valentin <valentin.faucon@gmail.com>
2020-01-17 18:00:22 +0100

    path changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import CampaignsPage from "./pages/CampaignsPage"
 import Ambassadors from "./pages/Ambassadors";
 import AmbassadorInfos from "./pages/AmbassadorInfos";
 import Causes from "./pages/Causes";
 import CauseInfos from "./pages/CauseInfos";
 import Footer from "./components/Footer";
 import "./App.scss";
-import CampaignPage from "./pages/CampaignPage";
+import CampaignPageById from "./pages/CampaignPageById";
 const { siteTitle } = require("./conf");
 
 function App() {
@@ -19,7 +20,8 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home}></Route>
-        <Route path="/campaign/:id" component={CampaignPage}></Route>
+        <Route path="/campaigns" component={CampaignsPage}></Route>
+        <Route path="/campaign/:id" component={CampaignPageById}></Route>
         <Route path="/ambassadors/:id" component={AmbassadorInfos}></Route>
         <Route path="/ambassadors" component={Ambassadors}></Route>
         <Route path="/causes/:id" component={CauseInfos}></Route>
