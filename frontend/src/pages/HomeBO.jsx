2916fac8ca2736ae200bf8320911b4c8571a9f03
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-20 17:52:08 +0100

    create new components for home admin part wait form login

---
+++ frontend/src/pages/HomeBO.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBarBO from "../components/NavBarBO";
+import CampaignsPageBO from "./CampaignsPageBO";
+import AmbassadorsPageBO from "./AmbassadorsPageBO";
+import CausesPageBO from "./CausesPageBO";
+import "./styles/HomeBO.scss";
+
+export default function HomeBO() {
+  return (
+    <div className="HomeBO">
+      <Switch>
+        <Route exact path="/campaignsBO" component={CampaignsPageBO}></Route>
+        <Route path="/ambassadorsBO" component={AmbassadorsPageBO}></Route>
+        <Route path="/causesBO" component={CausesPageBO}></Route>
+      </Switch>
+    </div>
+  );
+}
