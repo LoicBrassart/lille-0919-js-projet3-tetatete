bb5c4d5466ccc3515413bf4a5166771154f2bb0e
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-20 17:38:02 +0100

    adding new route for admin part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Ambassadors from "./pages/Ambassadors";
 import AmbassadorInfos from "./pages/AmbassadorInfos";
 import Causes from "./pages/Causes";
 import CauseInfos from "./pages/CauseInfos";
 import Footer from "./components/Footer";
+import HomeBO from "./pages/HomeBO";
 import "./App.scss";
 import CampaignPage from "./pages/CampaignPage";
+import NavBarBO from "./components/NavBarBO";
+
 const { siteTitle } = require("./conf");
 
 function App() {
   document.title = siteTitle;
+  const location = useLocation();
 
   return (
     <div className="App">
-      <NavBar />
+      {location.pathname === "/admin" ||
+      location.pathname === "/campaignsBO" ||
+      location.pathname === "/ambassadorsBO" ||
+      location.pathname === "/causesBO" ? (
+        <NavBarBO />
+      ) : (
+        <NavBar />
+      )}
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route path="/campaign/:id" component={CampaignPage}></Route>
@@ -24,8 +35,16 @@ function App() {
         <Route path="/ambassadors" component={Ambassadors}></Route>
         <Route path="/causes/:id" component={CauseInfos}></Route>
         <Route path="/causes" component={Causes}></Route>
+        <Route path="/admin" component={HomeBO}></Route>
       </Switch>
-      <Footer />
+      {location.pathname === "/admin" ||
+      location.pathname === "/campaignsBO" ||
+      location.pathname === "/ambassadorsBO" ||
+      location.pathname === "/causesBO" ? (
+        ""
+      ) : (
+        <Footer />
+      )}
     </div>
   );
 }
