f300eabb2d817dcff48b2d19c592399306f7e3a2
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-21 16:58:10 +0100

    adding filter

--- frontend/src/pages/Ambassadors.jsx
+++ frontend/src/pages/Ambassadors.jsx
@@ -3,10 +3,12 @@ import "./styles/Ambassadors.scss";
 import FilterTab from "../components/FilterTab";
 import ProfileCard from "../components/ProfileCard";
 import axios from "axios";
+import { useSelector } from "react-redux";
 const { apiCall } = require("../conf");
 
 function Ambassadors() {
   const [ambassadors, setAmbassadors] = useState([]);
+  const filter = useSelector(state => state.filter);
 
   useEffect(() => {
     axios.get(`${apiCall}/ambassador`).then(res => {
@@ -18,19 +20,27 @@ function Ambassadors() {
     <section className="Ambassadors">
       <FilterTab />
       <div className="center containerProfile">
-        {ambassadors.map(ambassador => {
-          return (
-            <ProfileCard
-              key={ambassador.id}
-              id={ambassador.id}
-              img={ambassador.img}
-              url="ambassadors"
-              style="blueContent"
-              mainStyle="ProfileCardBlue"
-              name={`${ambassador.firstname} ${ambassador.lastname}`}
-            />
-          );
-        })}
+        {ambassadors
+          .filter(ambassadors => {
+            if (filter === null) {
+              return ambassadors;
+            } else {
+              return ambassadors.tagList === filter;
+            }
+          })
+          .map(ambassador => {
+            return (
+              <ProfileCard
+                key={ambassador.id}
+                id={ambassador.id}
+                img={ambassador.img}
+                url="ambassadors"
+                style="blueContent"
+                mainStyle="ProfileCardBlue"
+                name={`${ambassador.firstname} ${ambassador.lastname}`}
+              />
+            );
+          })}
       </div>
     </section>
   );
