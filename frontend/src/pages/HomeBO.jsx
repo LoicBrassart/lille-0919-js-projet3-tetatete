4f19c1889ca3473e3e7d689dfeaf6c4390a8d8a4
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-21 11:53:03 +0100

    delete route and call in app.js

--- frontend/src/pages/HomeBO.jsx
+++ frontend/src/pages/HomeBO.jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import CampaignsPageBO from "./CampaignsPageBO";
-import AmbassadorsPageBO from "./AmbassadorsPageBO";
-import CausesPageBO from "./CausesPageBO";
 import "./styles/HomeBO.scss";
 
 export default function HomeBO() {
-  return (
-    <div className="HomeBO">
-      <Switch>
-        <Route exact path="/campaignsBO" component={CampaignsPageBO}></Route>
-        <Route path="/ambassadorsBO" component={AmbassadorsPageBO}></Route>
-        <Route path="/causesBO" component={CausesPageBO}></Route>
-      </Switch>
-    </div>
-  );
+  return <div className="HomeBO"></div>;
 }
