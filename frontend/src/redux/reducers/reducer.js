f620c1ffd8f649a5ff33f94e2c3fd9a28497784e
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-21 16:58:17 +0100

    adding reducer

---
+++ frontend/src/redux/reducers/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+  filter: null
+};
+
+function reducer(state = initialState, action) {
+  const newState = { ...state };
+  switch (action.type) {
+    case "SANTE":
+      return { filter: 1 };
+    case "HUMANITAIRE":
+      return { filter: 2 };
+    case "CULTURE":
+      return { filter: 3 };
+    case "SPORT":
+      return { filter: 4 };
+    case "ECOLOGIE":
+      return { filter: 5 };
+    case "EDUCATION":
+      return { filter: 6 };
+    default:
+      return newState;
+  }
+}
+export default reducer;
