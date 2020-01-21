07d97ac103bbdf70b9394c7a1ebe159842c9bbd4
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-21 16:57:30 +0100

    adding call of redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./reset.scss";
 import { BrowserRouter } from "react-router-dom";
+import reducer from "./redux/reducers/reducer";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
