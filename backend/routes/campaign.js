2b04ef92f67d0cce8a4e72a896a0547e6b73d5c3
Saulnier <bastien.saul@gmail.com>
2020-01-21 15:08:31 +0100

    get lastest campaign route

--- backend/routes/campaign.js
+++ backend/routes/campaign.js
@@ -2,12 +2,13 @@ const { connection } = require("../conf");
 const express = require("express");
 const router = express.Router();
 
-//Get all campaigns in progress order by their starting times
+//Get all campaigns in progress order by the most imminent campaign to finish
 //Or all campaigns done order by their ending times
-//Or all campaigns order by the most imminent campaign to finish
+//Or all campaigns where an ambassador or an association has participed
 //You can define a limit too
 router.get("/", (req, res) => {
   let sql = `SELECT 
+  campaign.id,
   campaign.name AS name, 
   campaign.img, 
   campaign.resume, 
@@ -23,24 +24,37 @@ router.get("/", (req, res) => {
   timediff(time_end,NOW()) AS timeDiff, 
   datediff(time_end,NOW()) AS dateDiff, 
   association.name AS associationName,
-  (time_to_sec(timediff(time_end, NOW())))/60 AS minuteRemaining
-    FROM campaign 
-    JOIN association ON campaign.id_association=association.id`;
+  (time_to_sec(timediff(time_end, NOW())))/60 AS minuteRemaining,
+  SUM(donation.donation_value) AS totalDonation
+    FROM campaign
+    JOIN association ON campaign.id_association=association.id
+    JOIN ambassador ON campaign.id_ambassador=ambassador.id
+    LEFT JOIN donation ON campaign.id=donation.campaign_id`;
   let query = [];
   if (req.query.inProgress) {
-    sql += " WHERE NOW() < time_end ORDER BY time_start DESC";
+    sql +=
+      " WHERE NOW() < time_end GROUP BY campaign.id ORDER BY timediff(time_end,NOW()) ASC";
   } else if (req.query.done) {
-    sql += " WHERE NOW() > time_end ORDER BY time_end DESC";
-  } else if (req.query.finishing) {
-    sql += " WHERE NOW() < time_end ORDER BY timediff(time_end,NOW()) ASC";
+    sql +=
+      " WHERE NOW() > time_end GROUP BY campaign.id ORDER BY time_end DESC";
+  } else if (req.query.ambassadorId) {
+    sql += " WHERE id_ambassador = ? GROUP BY campaign.id";
+    query.push(Number(req.query.ambassadorId));
+  } else if (req.query.associationId) {
+    sql += " WHERE id_association = ? GROUP BY campaign.id";
+    query.push(Number(req.query.associationId));
+  } else {
+    sql += " GROUP BY campaign.id";
   }
   if (req.query.limit) {
     sql += " LIMIT ?";
     query.push(Number(req.query.limit));
   }
-  connection.query(sql, [query], (err, results) => {
+  connection.query(sql, query, (err, results) => {
     if (err)
-      return res.status(500).send("Error in obtaining campaign's infos !");
+      return res
+        .status(500)
+        .send("Error in obtaining campaign's infos !" + err);
     if (results.length === 0)
       return res
         .status(204)
@@ -71,7 +85,31 @@ router.get("/total", (req, res) => {
 router.get("/:id", (req, res) => {
   const id = req.params.id;
   connection.query(
-    "SELECT * FROM campaign WHERE id =?",
+    `SELECT 
+  campaign.id,
+  campaign.name AS name, 
+  campaign.img, 
+  campaign.resume, 
+  DATE_ADD(campaign.time_start, INTERVAL 1 HOUR) AS time_start, 
+  DATE_ADD(campaign.time_end, INTERVAL 1 HOUR) AS time_end,
+  campaign.date_event, 
+  campaign.value1, 
+  campaign.value2, 
+  campaign.value3, 
+  campaign.id_user, 
+  campaign.id_ambassador, 
+  campaign.id_association, 
+  timediff(time_end,NOW()) AS timeDiff, 
+  datediff(time_end,NOW()) AS dateDiff, 
+  association.name AS associationName,
+  (time_to_sec(timediff(time_end, NOW())))/60 AS minuteRemaining,
+  SUM(donation.donation_value) AS totalDonation
+    FROM campaign
+    JOIN association ON campaign.id_association=association.id
+    JOIN ambassador ON campaign.id_ambassador=ambassador.id
+    LEFT JOIN donation ON campaign.id=donation.campaign_id
+    WHERE campaign.id = ?
+    GROUP BY campaign.id`,
     [id],
     (err, results) => {
       if (err)
