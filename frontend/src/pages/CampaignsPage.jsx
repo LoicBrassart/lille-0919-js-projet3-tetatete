81de1138990067f1e6c87c0de19c253879e958be
valentin <valentin.faucon@gmail.com>
2020-01-23 15:47:00 +0100

    campaign page done

--- frontend/src/pages/CampaignsPage.jsx
+++ frontend/src/pages/CampaignsPage.jsx
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from "react";
 import CampaignCard from "../components/CampaignCard";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+
 import "./styles/CampaignsPage.scss";
 
 const { apiCall } = require("../conf");
 
-export default function CampaignPageById(props) {
-  const [campaignInfo, setCampaignInfo] = useState([]);
+export default function CampaignPage() {
+  const dispatch = useDispatch();
+  const campaigns = useSelector(state => state.campaignsInfo);
   const [totalCampaign, setTotalCampaign] = useState([]);
-  const [campaignStatus, setCampaignStatus] = useState("en cours");
-  const [inprogressSelector, setInProgressSelector] = useState(false);
-  const [doneSelector, setDoneSelector] = useState(false);
-  const [campaignStateRoute, setCampaignStateRoute] = useState(
-    `${apiCall}/campaign?inProgress=true`
-  );
+  const [showOngoing, toggleShowOngoing] = useState(true);
 
   useEffect(() => {
-    axios.get(campaignStateRoute).then(res => setCampaignInfo(res.data));
     axios
-      .get(`${apiCall}/campaign/total`)
+      .get(
+        `${apiCall}/campaign/total${
+          showOngoing ? "?inProgress=true" : "?done=true"
+        }`
+      )
       .then(res => setTotalCampaign(res.data[0]));
-  }, []);
+  }, [showOngoing]);
+
+  useEffect(() => {
+    axios.get(`${apiCall}/campaign`).then(({ data }) => {
+      dispatch({ type: "GET_CAMPAIGNS_INFO", data });
+    });
+  }, [dispatch]);
 
   return (
     <div className="campaignsPage">
@@ -30,34 +37,55 @@ export default function CampaignPageById(props) {
       </div>
 
       <div className="topContent">
-        <div className="SelectContent">
-          <div className="inProgressCheckBox">
-            <input type="checkbox" id="inProgressCampaignSelect" />
-            <label id="inProgress" for="inProgress" aria-describedby="label">
-              Rencontres en cours
-            </label>
+        <div className="selectContent">
+          <div
+            className={showOngoing ? "inProgressStatus" : "unselected"}
+            onClick={() => {
+              toggleShowOngoing(true);
+            }}
+          >
+            <div
+              className={showOngoing ? "inProgressStatus" : "unselected"}
+            ></div>
+            <p id="inProgress"> Rencontres en cours</p>
           </div>
-
-          <div className="finishCheckBox">
-            <input type="checkbox" id="finishCampaignSelect" />
-            <label id="finish" for="finish" aria-describedby="label">
-              Rencontres terminées
-            </label>
+          <div
+            className={showOngoing ? "unselected" : "doneStatus"}
+            onClick={() => {
+              toggleShowOngoing(false);
+            }}
+          >
+            <div className={showOngoing ? "unselected" : "doneStatus"}></div>
+            <p id="done">Rencontres terminées</p>
           </div>
         </div>
 
-        <div className="totalCampaign">
+        <div
+          className={
+            showOngoing ? "totalInprogressCampaign" : "totalDoneCampaign"
+          }
+        >
           <p className="totalCamp">{totalCampaign.total}</p>
           <p className="campaignStatus">
-            campagnes <br /> {campaignStatus}
+            campagnes <br /> {showOngoing ? "en cours" : "terminées"}
           </p>
         </div>
       </div>
 
       <div className="cardContainer">
-        {campaignInfo.map((campaign, key) => {
-          return <CampaignCard key={key} campaignInfo={campaign} />;
-        })}
+        {campaigns
+          .filter(campaign => {
+            return showOngoing === campaign.dateDiff > 0;
+          })
+          .map((campaign, key) => {
+            return (
+              <CampaignCard
+                classname={showOngoing ? "Inprogress" : "done"}
+                key={key}
+                {...campaign}
+              />
+            );
+          })}
       </div>
     </div>
   );
