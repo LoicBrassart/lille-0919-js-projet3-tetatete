9512dd6c2f2c8fbc97b03f432b88dd33b98fc92f
Saulnier <bastien.saul@gmail.com>
2020-01-23 16:02:36 +0100

    fixed buggy filtering actions via redux

--- frontend/src/redux/reducers/reducer.js
+++ frontend/src/redux/reducers/reducer.js
@@ -1,24 +1,39 @@
 const initialState = {
-  filter: null
+  filterAmbassadors: null,
+  filterCauses: null
 };
 
 function reducer(state = initialState, action) {
   const newState = { ...state };
   switch (action.type) {
-    case "SANTE":
-      return { filter: 1 };
-    case "HUMANITAIRE":
-      return { filter: 2 };
-    case "CULTURE":
-      return { filter: 3 };
-    case "SPORT":
-      return { filter: 4 };
-    case "ECOLOGIE":
-      return { filter: 5 };
-    case "EDUCATION":
-      return { filter: 6 };
-    case "ALL":
-      return { filter: null };
+    case "AMB_SANTE":
+      return { filterAmbassadors: 1, filterCauses: null };
+    case "AMB_HUMANITAIRE":
+      return { filterAmbassadors: 2, filterCauses: null };
+    case "AMB_CULTURE":
+      return { filterAmbassadors: 3, filterCauses: null };
+    case "AMB_SPORT":
+      return { filterAmbassadors: 4, filterCauses: null };
+    case "AMB_ECOLOGIE":
+      return { filterAmbassadors: 5, filterCauses: null };
+    case "AMB_EDUCATION":
+      return { filterAmbassadors: 6, filterCauses: null };
+    case "AMB_ALL":
+      return { filterAmbassadors: null, filterCauses: null };
+    case "ASS_SANTE":
+      return { filterCauses: 1, filterAmbassadors: null };
+    case "ASS_HUMANITAIRE":
+      return { filterCauses: 2, filterAmbassadors: null };
+    case "ASS_CULTURE":
+      return { filterCauses: 3, filterAmbassadors: null };
+    case "ASS_SPORT":
+      return { filterCauses: 4, filterAmbassadors: null };
+    case "ASS_ECOLOGIE":
+      return { filterCauses: 5, filterAmbassadors: null };
+    case "ASS_EDUCATION":
+      return { filterCauses: 6, filterAmbassadors: null };
+    case "ASS_ALL":
+      return { filterCauses: null, filterAmbassadors: null };
     default:
       return newState;
   }
