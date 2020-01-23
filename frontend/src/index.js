43ec2cbb6fede77a3981d7640e63092ffe7fb832
valentin <valentin.faucon@gmail.com>
2020-01-23 15:45:19 +0100

    redux devtools added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,12 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./redux/reducers/reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 
 ReactDOM.render(
   <Provider store={store}>
