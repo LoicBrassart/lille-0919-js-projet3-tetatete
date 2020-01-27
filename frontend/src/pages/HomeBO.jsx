376a0c02a841c1d0286e0b009fee6d2cbc17dcbf
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-27 17:54:46 +0100

    adding form create user

--- frontend/src/pages/HomeBO.jsx
+++ frontend/src/pages/HomeBO.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import FormCreateUser from "../components/FormCreateUser";
+import FormLoginBO from "../components/FormLoginBO";
 import "./styles/HomeBO.scss";
+import { useSelector } from "react-redux";
 
 export default function HomeBO() {
-  return <div className="HomeBO"></div>;
+  const isLogged = useSelector(state => state.isLogged);
+
+  return (
+    <div className="HomeBO">
+      <FormCreateUser />}
+    </div>
+  );
 }
