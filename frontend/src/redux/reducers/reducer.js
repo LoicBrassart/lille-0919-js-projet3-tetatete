0922e93fb70499a2f4004a88ba902bfc21750568
Saulnier <bastien.saul@gmail.com>
2020-01-28 15:35:28 +0100

    refactored reducer

--- frontend/src/redux/reducers/reducer.js
+++ frontend/src/redux/reducers/reducer.js
@@ -9,45 +9,19 @@ const reducer = (state = initialState, action) => {
   const newState = JSON.parse(JSON.stringify(state));
   switch (action.type) {
     case "GET_CAMPAIGNS_INFO":
-      return {
-        ...newState,
-        campaignsInfo: action.data
-      };
+      return { ...newState, campaignsInfo: action.data };
     case "GET_CAMPAIGN_BY_ID":
+      return { ...newState, campaignInfoById: action.data };
+    case "AMBASSADORS_FILTER":
+      return { ...newState, filterAmbassadors: action.payload };
+    case "CAUSES_FILTER":
+      return { ...newState, filterCauses: action.payload };
+    case "SWITCH":
       return {
         ...newState,
-        campaignInfoById: action.data
+        filterAmbassadors: action.payload,
+        filterCauses: action.payload
       };
-    case "AMB_SANTE":
-      return { ...newState, filterAmbassadors: 1, filterCauses: null };
-    case "AMB_HUMANITAIRE":
-      return { ...newState, filterAmbassadors: 2, filterCauses: null };
-    case "AMB_CULTURE":
-      return { ...newState, filterAmbassadors: 3, filterCauses: null };
-    case "AMB_SPORT":
-      return { ...newState, filterAmbassadors: 4, filterCauses: null };
-    case "AMB_ECOLOGIE":
-      return { ...newState, filterAmbassadors: 5, filterCauses: null };
-    case "AMB_EDUCATION":
-      return { ...newState, filterAmbassadors: 6, filterCauses: null };
-    case "AMB_ALL":
-      return { ...newState, filterAmbassadors: null, filterCauses: null };
-    case "ASS_SANTE":
-      return { ...newState, filterCauses: 1, filterAmbassadors: null };
-    case "ASS_HUMANITAIRE":
-      return { ...newState, filterCauses: 2, filterAmbassadors: null };
-    case "ASS_CULTURE":
-      return { ...newState, filterCauses: 3, filterAmbassadors: null };
-    case "ASS_SPORT":
-      return { ...newState, filterCauses: 4, filterAmbassadors: null };
-    case "ASS_ECOLOGIE":
-      return { ...newState, filterCauses: 5, filterAmbassadors: null };
-    case "ASS_EDUCATION":
-      return { ...newState, filterCauses: 6, filterAmbassadors: null };
-    case "ASS_ALL":
-      return { ...newState, filterAmbassadors: null, filterCauses: null };
-    case "SWITCH":
-      return { ...newState, filterCauses: null, filterAmbassadors: null };
     default:
       return newState;
   }
