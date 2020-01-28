31d81316dbf7c7f7dc040683dfabb1858bca0e27
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-28 16:35:52 +0100

    delete all useless

--- frontend/src/pages/HomeBO.jsx
+++ frontend/src/pages/HomeBO.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import FormCreateUser from "../components/FormCreateUser";
-import FormLoginBO from "../components/FormLoginBO";
 import "./styles/HomeBO.scss";
-import { useSelector } from "react-redux";
 
 export default function HomeBO() {
-
   return (
     <div className="HomeBO">
-      <FormCreateUser />}
+      <FormCreateUser />
     </div>
   );
 }
