9b26f0b81690650c6c22be0b3a88cdda63b289c0
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-01-31 12:17:01 +0100

    adding message to isAdmin or not

--- frontend/src/pages/HomeBO.jsx
+++ frontend/src/pages/HomeBO.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import FormCreateUser from "../components/FormCreateUser";
 import "./styles/HomeBO.scss";
 
 export default function HomeBO() {
+  const isAdmin = useSelector(state => state.dataJWT.user[0].canAdmin);
+
   return (
     <div className="HomeBO">
-      <FormCreateUser />
+      {isAdmin === 1 ? (
+        (<p>
+          Vous êtes bien connecté. Vous pouvez créer une/des campagne(s),
+          ajouter un/des ambassadeur et une/des association(s)
+        </p>)(<FormCreateUser />)
+      ) : (
+        <p>
+          Vous êtes bien connecté. Vous pouvez ajouter un ambassadeurs et créer
+          des campagnes.
+        </p>
+      )}
     </div>
   );
 }
