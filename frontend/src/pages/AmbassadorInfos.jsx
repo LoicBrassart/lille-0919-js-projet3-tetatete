299f304d7128e42c669f9d975f83d82cba866a06
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-02-04 15:39:18 +0100

    adding key to resolved warnings

--- frontend/src/pages/AmbassadorInfos.jsx
+++ frontend/src/pages/AmbassadorInfos.jsx
@@ -55,9 +55,9 @@ function AmbassadorInfos(props) {
                   <p className="emptyTag">Pas de tags associés.</p>
                 </li>
               ) : (
-                tags.map(tag => {
+                tags.map((tag, key) => {
                   return (
-                    <li>
+                    <li key={key}>
                       <img src={`/img/${tag}.png`} alt="" />
                     </li>
                   );
@@ -85,9 +85,9 @@ function AmbassadorInfos(props) {
           <p className="emptyCards">Pas de liens disponibles.</p>
         ) : relatedCampaigns.length <= 3 && relatedCampaigns.length !== 0 ? (
           <Carousel itemWidth={400}>
-            {relatedCampaigns.map(relatedCampaign => {
+            {relatedCampaigns.map((relatedCampaign, key) => {
               return (
-                <div>
+                <div key={key}>
                   <CampaignCard key={relatedCampaign.id} {...relatedCampaign} />
                 </div>
               );
@@ -95,9 +95,9 @@ function AmbassadorInfos(props) {
           </Carousel>
         ) : (
           <Carousel arrows itemWidth={400}>
-            {relatedCampaigns.map(relatedCampaign => {
+            {relatedCampaigns.map((relatedCampaign, key) => {
               return (
-                <div>
+                <div key={key}>
                   <CampaignCard key={relatedCampaign.id} {...relatedCampaign} />
                 </div>
               );
