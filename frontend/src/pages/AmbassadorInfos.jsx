a9fc5dac054e4b9b15f6f5be500e605a3924c5d3
LeoDiduch <leo-diduch_student2019@wilder.school>
2020-02-04 14:35:15 +0100

    resolve warnings

--- frontend/src/pages/AmbassadorInfos.jsx
+++ frontend/src/pages/AmbassadorInfos.jsx
@@ -24,7 +24,7 @@ function AmbassadorInfos(props) {
     axios
       .get(`${apiCall}/campaign?ambassadorId=${props.match.params.id}`)
       .then(res => {
-        if (res.data != []) {
+        if (res.data !== []) {
           setRelatedCampaigns(res.data);
           setRelatedDatas(true);
         } else {
@@ -83,7 +83,7 @@ function AmbassadorInfos(props) {
       <div className="AmbassadorsRelated">
         {relatedCampaigns.length === 0 ? (
           <p className="emptyCards">Pas de liens disponibles.</p>
-        ) : relatedCampaigns.length <= 3 && relatedCampaigns.length != 0 ? (
+        ) : relatedCampaigns.length <= 3 && relatedCampaigns.length !== 0 ? (
           <Carousel itemWidth={400}>
             {relatedCampaigns.map(relatedCampaign => {
               return (
